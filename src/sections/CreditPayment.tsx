cad2a4506ce782af4e0b97f983e5c5513d520e84
Pedro Lugon <pedromotta20@gmail.com>
2024-07-11 06:00:16 -0300

    feat(credit): add textfields and select

--- src/sections/CreditPayment.tsx
+++ src/sections/CreditPayment.tsx
@@ -1,10 +1,21 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 import { BillingInfo } from "../components/BillingInfo";
 import { Button } from "../components/Button";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { BillingStore } from "../contexts/BillingStore";
+import { formatNumberToLocale } from "../utils/formatNumberToLocale";
 
 export default function CreditPayment() {
+  const [name, setName] = useState<string>("");
+
   const { billing } = BillingStore();
 
   function nextStep() {}
@@ -16,6 +27,29 @@ export default function CreditPayment() {
         João, pague o restante em {billing.parcels.length}x no cartão
       </h2>
       <div className="flex flex-col gap-4 items-center px-4">
+        <TextField
+          label="Nome completo"
+          variant="outlined"
+          fullWidth
+          defaultValue={"teste"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField label="CPF" variant="outlined" fullWidth />
+        <TextField label="Número do cartão" variant="outlined" fullWidth />
+        <div className="flex w-full flex-row justify-between gap-4">
+          <TextField label="Vencimento" variant="outlined" fullWidth />
+          <TextField label="CVV" variant="outlined" fullWidth />
+        </div>
+        <FormControl fullWidth>
+          <InputLabel>Parcelas</InputLabel>
+          <Select label="Parcelas" defaultValue={1}>
+            <MenuItem value={1}>
+              {billing.parcels.length}x de{" "}
+              {formatNumberToLocale(billing.parcels[0])}
+            </MenuItem>
+          </Select>
+        </FormControl>
         <Button onClick={() => nextStep()} className="w-full">
           Pagar
         </Button>
