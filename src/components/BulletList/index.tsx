ab6aa81157bd432dc41176cd54fa805c7b79dccb
Pedro Lugon <pedromotta20@gmail.com>
2024-07-11 05:34:43 -0300

    fix(bullet list): add border to card

--- src/components/BulletList/index.tsx
+++ src/components/BulletList/index.tsx
@@ -1,3 +1,4 @@
+import { BillingStore } from "../../contexts/BillingStore";
 import { formatNumberToLocale } from "../../utils/formatNumberToLocale";
 import "./index.css";
 
@@ -6,11 +7,26 @@ interface BulletListProps {
 }
 
 export function BulletList({ parcels }: BulletListProps) {
+  const { billing } = BillingStore();
+
   return (
     <div>
       {parcels.map((value, index) => (
-        <li className="flex flex-row justify-between items-center">
-          <p className="text-[16px]">{index + 1}ª entrada no Pix</p>
+        <li
+          className={
+            billing.pix
+              ? "flex flex-row justify-between items-center green-border"
+              : "flex flex-row justify-between items-center"
+          }
+        >
+          <p className="text-[16px]">
+            {index + 1}ª{" "}
+            {parcels.length === 1
+              ? "parcela no Pix"
+              : index === 0
+              ? "entrada no Pix"
+              : "parcela no Cartão"}
+          </p>
           <p className="text-[16px] font-extrabold">
             {formatNumberToLocale(value)}
           </p>
