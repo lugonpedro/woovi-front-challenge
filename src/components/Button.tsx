daffd69f04f373d75cccd9b546f127aded1f354b
Pedro Lugon <pedromotta20@gmail.com>
2024-07-11 05:35:03 -0300

    feat(button): add custom classname

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,11 @@ import { ComponentProps } from "react";
 
 interface ButtonProps extends ComponentProps<"button"> {}
 
-export function Button({ children, onClick }: ButtonProps) {
+export function Button({ children, onClick, className }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-[#133A6F] text-white font-semibold text-[16px] rounded-xl py-2 px-4 flex flex-row gap-2"
+      className={`bg-[#133A6F] text-white text-center justify-center font-semibold text-[16px] rounded-xl py-2 px-4 flex flex-row gap-2 ${className}`}
     >
       {children}
     </button>
