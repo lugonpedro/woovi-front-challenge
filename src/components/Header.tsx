4517ee341d8ef257379885c44a4c6a087a5d8b45
Pedro Lugon <pedromotta20@gmail.com>
2024-07-11 16:55:56 -0300

    fix(header): change margin to padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.png";
 
 export function Header() {
   return (
-    <div className="flex justify-center items-center my-4">
+    <div className="flex justify-center items-center py-4">
       <img src={logo} alt="Woovi logo in green color" />
     </div>
   );
