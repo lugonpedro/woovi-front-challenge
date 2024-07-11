269a99594a8a720d3378d9abd999b21bb84de5b0
Pedro Lugon <pedromotta20@gmail.com>
2024-07-11 05:12:31 -0300

    fix(pix): improving billing info

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -3,7 +3,7 @@ import chevronUpIcon from "../assets/chevron.png";
 export function HowItWorks() {
   return (
     <div className="flex flex-row justify-between items-center">
-      <p className="font-extrabold">Como funciona?</p>
+      <p className="font-extrabold text-[#4D4D4D]">Como funciona?</p>
       <img src={chevronUpIcon} />
     </div>
   );
